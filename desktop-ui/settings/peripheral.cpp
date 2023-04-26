ce3c1a7782694e159eab28eb478ab2c39d09d9d5
kev4cards <21962903+kev4cards@users.noreply.github.com>
2023-04-25 21:16:30 -0400

    Update and rename n64stick.cpp to peripheral.cpp

---
+++ desktop-ui/settings/peripheral.cpp
@@ -0,0 +1,15 @@
+auto PeripheralSettings::construct() -> void {
+  setCollapsible();
+  setVisible(false);
+
+  controlStickAdjustmentLabel.setText("N64 Control Stick Adjustment").setFont(Font().setBold());
+  controlStickAdjustmentLayout.setSize({3, 3}).setPadding(12_sx, 0);
+  controlStickAdjustmentLayout.column(0).setAlignment(1.0);
+
+  stickRangeLabel.setText("Range:");
+  stickRangeValue.setAlignment(0.5);
+  stickRangeSlider.setLength(86).setPosition(settings.peripheral.stickRange * 85.0).onChange([&] {
+    settings.peripheral.stickRange = stickRangeSlider.position() / 85.0;
+    stickRangeValue.setText(stickRangeSlider.position());
+  }).doChange();
+}
